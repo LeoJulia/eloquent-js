d5b8caa7a4119c22a7cfe0a8a8f7495efa988473
Yeine <mornemira@yandex.ru>
2017-03-15 21:08:44 +0300

    carry out task Recurtion

--- 4_Recursion.js
+++ 4_Recursion.js
@@ -18,3 +18,17 @@
     console.log(isEven(-1));
     // → ??
 */
+
+function isEven (x) {
+    if (x == 0){
+        return "true";
+    } else if (x == 1){
+        return "false";
+    } else if (x < 0){
+        return isEven(-x);
+    } else {
+        return isEven(x-2);
+    }
+}
+
+console.log(isEven(-2));
